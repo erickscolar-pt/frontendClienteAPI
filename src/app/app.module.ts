58e39ce728bbbdc65f2fe6e40a035267b49ec009
Erick Scolar <erick.scolar@outlook.com>
2022-02-04 17:11:28 -0300

    Primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
+import { ClientesModule } from './clientes/clientes.module';
+import { ClientesService } from './clientes.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    TemplateModule,
+    ClientesModule
+  ],
+  providers: [
+    ClientesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+import { ClientesService } from 'src/app/clientes.service';
+import { Cliente } from '../cliente';
+
+@Component({
+  selector: 'app-clientes-lista',
+  templateUrl: './clientes-lista.component.html',
+  styleUrls: ['./clientes-lista.component.css']
+})
+export class ClientesListaComponent implements OnInit {
+
+  clientes: Cliente[] = [];
+  clienteSelecionado: Cliente;
+  mensagemSucesso: string;
+  mensagemErro: string;
+
+
+  constructor(
+    private service : ClientesService, 
+    private router: Router
+    ){}
+
+  ngOnInit(): void {
+    this.service 
+    .getClientes()
+    .subscribe( resposta => this.clientes = resposta );
+  }
+
+  novoCadastro(){
+    this.router.navigate(['/clientes-form'])
+  }
+
+  preparaDelecao(cliente: Cliente){
+    this.clienteSelecionado = cliente;
+  }
+
+  deletarCliente(){
+
+    this.service
+    .deletar(this.clienteSelecionado)
+    .subscribe( response => {
+      this.mensagemSucesso = 'Cliente deletado com sucesso'
+    this.ngOnInit();
+  },
+    error => this.mensagemErro = 'Erro ao deletar o cliente' )
+  }
+
+}
